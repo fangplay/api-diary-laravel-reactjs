c54761240165d1ef325d68962926a260206807c0
fangplay <fangplay_2014@hotmail.com>
2023-11-23 20:37:42 +0700

    create & change migration

--- database/migrations/2023_11_23_133118_post_story.php
+++ database/migrations/2023_11_23_133118_post_story.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('long_stories');
             $table->timestamps();
         });
+        //
     }
 
     /**
@@ -27,6 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::drop('stories');
     }
 };
